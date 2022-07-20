0a1429337eb738039e0adf8caf6709fa87c95987
Dylan Hays <twentygx@outlook.com>
2022-07-19 20:56:30 -0500

    commit tree 95781a65e3ea40a12c0950daf0dbd8dd9bc5f30f

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    let cardArray = [];
+    for (let i = 0; i < names.length; i++)
+    {
+        cardArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return cardArray;
+}
+
+function countDown(i) {
+    while (i >= 0)
+    {
+        console.log(i--);
+    }
+}
